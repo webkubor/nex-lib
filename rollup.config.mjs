1f30f37d3539185310ad5a0446d9ac11655dc9b9
webkubor <webkubor@163.com>
2024-09-12 18:10:38 +0800

    Feat 优化

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { dirname } from 'path';
+import { terser } from '@rollup/plugin-terser'; // 导入 terser 插件
 const aliases = {
     resolve: [
       // 设置别名
@@ -30,5 +31,6 @@ export default {
       }),
     commonjs(),
     typescript(),
+    terser() // 添加 terser 插件以压缩代码
   ],
 };
