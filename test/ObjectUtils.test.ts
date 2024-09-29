6ccd0682a4e376cae4d52bb53032ac62c7aa0113
webkubor <webkubor@163.com>
2024-09-29 12:04:14 +0800

    feat (ui): add<beginning of the code>

--- test/ObjectUtils.test.ts
+++ test/ObjectUtils.test.ts
@@ -4,17 +4,16 @@ import ObjectUtils from '../src/ObjectUtils'
 
 describe('test ObjectUtils', () => {
     it('judgeTypes', () => {
-        EchoUtils.green(`123456`, ObjectUtils.judgeTypes(123456))
-        EchoUtils.green(`[]`, ObjectUtils.judgeTypes([]))
-        EchoUtils.green(`new Date()`, ObjectUtils.judgeTypes(new Date()))
-        EchoUtils.yellow(`true`, ObjectUtils.judgeTypes(true))
-        EchoUtils.purple(`hello`, ObjectUtils.judgeTypes('hello'))
-        EchoUtils.orange(`function() {}`, ObjectUtils.judgeTypes(function() {}))
-        EchoUtils.pink(`new Error('something wrong')`, ObjectUtils.judgeTypes(new Error('something wrong')))
-        EchoUtils.creamWhite(`null`, ObjectUtils.judgeTypes(null))
-        EchoUtils.bgGreen(`undefined`, ObjectUtils.judgeTypes(undefined))
-        EchoUtils.magenta(`/abc/`, ObjectUtils.judgeTypes(/abc/))
-        
+        EchoUtils.green(`123456`, ObjectUtils.judgeTypes(123456)) //  123456 number
+        EchoUtils.green(`[]`, ObjectUtils.judgeTypes([])) //    [] array
+        EchoUtils.green(`new Date()`, ObjectUtils.judgeTypes(new Date())) //    new Date() date
+        EchoUtils.yellow(`true`, ObjectUtils.judgeTypes(true)) //    true boolean
+        EchoUtils.purple(`hello`, ObjectUtils.judgeTypes('hello')) //  hello string
+        EchoUtils.orange(`function() {}`, ObjectUtils.judgeTypes(function() {})) //  function() {} function
+        EchoUtils.pink(`new Error('something wrong')`, ObjectUtils.judgeTypes(new Error('something wrong'))) // new Error('something wrong') error
+        EchoUtils.creamWhite(`null`, ObjectUtils.judgeTypes(null)) // null null
+        EchoUtils.bgGreen(`undefined`, ObjectUtils.judgeTypes(undefined)) //    undefined undefined
+        EchoUtils.magenta(`/abc/`, ObjectUtils.judgeTypes(/abc/)) //  /abc/ regexp
     });
 
 });
